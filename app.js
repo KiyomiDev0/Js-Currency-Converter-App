f70ce93829f567a86c27c1e22d05436dad7001be
KiyomiDev <amal.97mo@gmail.com>
2022-04-25 04:17:00 +0200

    Get currency code of all country

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let selectBox = document.querySelector(".select-box"),
-    from = document.getElementById("from")
+    from = document.getElementById("from"),
+    to = document.getElementById("to"),
+    input = document.querySelector("input"),
+    output = document.getElementById("output"),
+    btn = document.querySelector("button");
 
-
+getCurrencyCode();
 
 // Change select box background color on focus
 from.addEventListener("focus", () => {
@@ -12,3 +16,20 @@ from.addEventListener("blur", () => {
    selectBox.style.backgroundColor = "#586376";
    from.style.color = "#fff";
 })
+
+// Getting currency code of all country and inserting it inside select tag
+async function getCurrencyCode() {
+   let res = await fetch(`https://v6.exchangerate-api.com/v6/7af49191bf482e5bd2719106/latest/AUD`),
+       resData = await res.json();
+   let names = Array.from(Object.getOwnPropertyNames(resData.conversion_rates))
+   names.forEach((name, index) => {
+      name = names[index];
+      // Set USD and EGP as default inputs
+      if (name == 'USD') {
+         from.innerHTML += `<option value="${name}" selected>${name}</option>`;
+      } else from.innerHTML += `<option value="${name}">${name}</option>`;
+      if (name == 'EGP') {
+         to.innerHTML += `<option value="${name}" selected>${name}</option>`;
+      } else to.innerHTML += `<option value="${name}">${name}</option>`;
+   })
+}
