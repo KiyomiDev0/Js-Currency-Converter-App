2ebf61a817803dead80db86fa8d2780521622295
KiyomiDev <amal.97mo@gmail.com>
2022-04-27 02:13:57 +0200

    Get the exchange rate on currency switch from the current currency data

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let selectBox = document.querySelector(".select-box"),
-    from = document.getElementById("from"),
-    to = document.getElementById("to"),
-    input = document.querySelector("input"),
-    output = document.getElementById("output"),
-    btn = document.querySelector("button"),
-    dropLists = Array.from(document.getElementsByTagName("select")),
-    imgs = document.getElementsByTagName("img");
+   from = document.getElementById("from"),
+   to = document.getElementById("to"),
+   input = document.querySelector("input"),
+   output = document.getElementById("output"),
+   btn = document.querySelector("button"),
+   dropLists = Array.from(document.getElementsByTagName("select")),
+   imgs = document.getElementsByTagName("img"),
+   switchIcon = document.querySelector(".exchange-icon");
 
 getCurrencyCode();
 
@@ -45,22 +46,43 @@ async function getCurrencyCode() {
 
 // Get Exchange Rate
 btn.addEventListener("click", () => {
+   getExchangeRate();
+})
+
+
+// Reversing currency codes on icon click
+let lastRate;
+
+switchIcon.addEventListener("click", () => {
+   [from.value, to.value] = [to.value, from.value];
+   if (lastRate != undefined) {
+      lastRate = input.value / (input.value * lastRate); // 22 / (22 * 18.58)
+      output.innerText = `${input.value} ${from.value} = ${(input.value * lastRate).toFixed(2)} ${to.value}`;
+   }
+   else getExchangeRate();
+   // Reversing currency flags
+   [imgs[0].src, imgs[1].src] = [imgs[1].src, imgs[0].src];
+})
+
+// Get Exchange Rate Function
+function getExchangeRate() {
    if (input.value == '' || input.value == 0) {
       output.style.color = "red";
       output.innerText = "Enter amount and try again";
    } else {
       fetch(`https://v6.exchangerate-api.com/v6/7af49191bf482e5bd2719106/latest/${from.value}`)
-      .then(response => response.json())
-      .then(data => {
-       let conversionRate = data.conversion_rates[to.value],
-           result = (input.value * conversionRate).toFixed(2);
-       output.style.color = "#586376";
-       output.style.fontSize = "14px";
-       output.innerText = "Get Exchange Rate...";
-       setTimeout(() => {
-          window.innerWidth < 365 ? output.style.fontSize = "18px" : output.style.fontSize = "25px";
-          output.innerText = `${input.value} ${from.value} = ${result} ${to.value}`;
-       }, 800);
-      });
+         .then(response => response.json())
+         .then(data => {
+            let conversionRate = data.conversion_rates[to.value],
+               result = (input.value * conversionRate).toFixed(2);
+            lastRate = conversionRate;
+            output.style.color = "#586376";
+            output.style.fontSize = "14px";
+            output.innerText = "Get Exchange Rate...";
+            setTimeout(() => {
+               window.innerWidth < 365 ? output.style.fontSize = "18px" : output.style.fontSize = "25px";
+               output.innerText = `${input.value} ${from.value} = ${result} ${to.value}`;
+            }, 800);
+         });
    }
-})
+}
